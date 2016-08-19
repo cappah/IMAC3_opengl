3ec459d91f5cef2f0ebab9a4030ec954b6a69efe
florianLequertier <florian.lequertier@free.fr>
2016-08-19 20:09:59 +0200

    Modification du InputHandler, pour gerer mieux le focus.

--- tp02/InputHandler.cpp
+++ tp02/InputHandler.cpp
@@ -1,47 +1,18 @@
 #include "InputHandler.h"
 
+GLFWwindow* InputHandler::m_window = nullptr;
+std::map<int, bool> InputHandler::previousMouseState;
+std::map<int, bool> InputHandler::previousKeyboardState;
+InputHandler::FocusStates InputHandler::m_focusState = InputHandler::FOCUSING_ALL;
+
+
+InputHandler::InputHandler()
+{
+	setFocusState(FocusStates::FOCUSING_ALL);
+	attachToWindow(nullptr);
+}
 
 void InputHandler::synchronize()
-{
-	get()._synchronize();
-}
-
-bool InputHandler::getMouseButtonUp(int button)
-{
-	return get()._getMouseButtonUp(button);
-}
-
-bool InputHandler::getMouseButton(int button)
-{
-	return get()._getMouseButton(button);
-}
-
-bool InputHandler::getMouseButtonDown(int button)
-{
-	return get()._getMouseButtonDown(button);
-}
-
-bool InputHandler::getKeyUp(int key)
-{
-	return get()._getKeyUp(key);
-}
-
-bool InputHandler::getKey(int key)
-{
-	return get()._getKey(key);
-}
-
-bool InputHandler::getKeyDown(int key)
-{
-	return get()._getKeyDown(key);
-}
-
-void InputHandler::attachToWindow(GLFWwindow * window)
-{
-	get()._attachToWindow(window);
-}
-
-void InputHandler::_synchronize()
 {
 	for (int i = 0; i < GLFW_MOUSE_BUTTON_LAST; i++)
 	{
@@ -53,37 +24,153 @@ void InputHandler::_synchronize()
 	}
 }
 
-bool InputHandler::_getMouseButtonUp(int button)
+bool InputHandler::getMouseButtonUp(int button, FocusStates focusContext)
 {
-	return (!glfwGetMouseButton(m_window, button) && previousMouseState[button]);
+	return (!glfwGetMouseButton(m_window, button) && previousMouseState[button]) && ((focusContext & m_focusState) != FocusStates::FOCUSING_NONE);
 }
 
-bool InputHandler::_getMouseButton(int button)
+bool InputHandler::getMouseButton(int button, FocusStates focusContext)
 {
-	return glfwGetMouseButton(m_window, button);
+	return glfwGetMouseButton(m_window, button) && ((focusContext & m_focusState) != FocusStates::FOCUSING_NONE);
 }
 
-bool InputHandler::_getMouseButtonDown(int button)
+bool InputHandler::getMouseButtonDown(int button, FocusStates focusContext)
 {
-	return (glfwGetMouseButton(m_window, button) && !previousMouseState[button]);
+	return (glfwGetMouseButton(m_window, button) && !previousMouseState[button]) && ((focusContext & m_focusState) != FocusStates::FOCUSING_NONE);
 }
 
-bool InputHandler::_getKeyUp(int key)
+bool InputHandler::getKeyUp(int key, FocusStates focusContext)
 {
-	return (!glfwGetKey(m_window, key) && previousKeyboardState[key]);
+	return (!glfwGetKey(m_window, key) && previousKeyboardState[key]) && ((focusContext & m_focusState) != FocusStates::FOCUSING_NONE);
 }
 
-bool InputHandler::_getKey(int key)
+bool InputHandler::getKey(int key, FocusStates focusContext)
 {
-	return glfwGetKey(m_window, key);
+	return glfwGetKey(m_window, key) && ((focusContext & m_focusState) != FocusStates::FOCUSING_NONE);
 }
 
-bool InputHandler::_getKeyDown(int key)
+bool InputHandler::getKeyDown(int key, FocusStates focusContext)
 {
-	return (glfwGetKey(m_window, key) && !previousKeyboardState[key]);
+	return (glfwGetKey(m_window, key) && !previousKeyboardState[key]) && ((focusContext & m_focusState) != FocusStates::FOCUSING_NONE);
 }
 
-void InputHandler::_attachToWindow(GLFWwindow * window)
+InputHandler::FocusStates InputHandler::getFocusState()
+{
+	return m_focusState;
+}
+
+void InputHandler::setFocusState(FocusStates state)
+{
+	m_focusState = state;
+}
+
+void InputHandler::attachToWindow(GLFWwindow * window)
 {
 	m_window = window;
 }
+
+//void InputHandler::synchronize()
+//{
+//	get()._synchronize();
+//}
+//bool InputHandler::getMouseButtonUp(int button, FocusStates focusContext)
+//{
+//	return get()._getMouseButtonUp(button);
+//}
+//
+//bool InputHandler::getMouseButton(int button, FocusStates focusContext)
+//{
+//	return get()._getMouseButton(button);
+//}
+//
+//bool InputHandler::getMouseButtonDown(int button, FocusStates focusContext)
+//{
+//	return get()._getMouseButtonDown(button);
+//}
+//
+//bool InputHandler::getKeyUp(int key, FocusStates focusContext)
+//{
+//	return get()._getKeyUp(key);
+//}
+//
+//bool InputHandler::getKey(int key, FocusStates focusContext)
+//{
+//	return get()._getKey(key);
+//}
+//
+//bool InputHandler::getKeyDown(int key, FocusStates focusContext)
+//{
+//	return get()._getKeyDown(key);
+//}
+//
+//InputHandler::FocusStates InputHandler::getFocusState()
+//{
+//	return get()._getFocusState();
+//}
+//
+//void InputHandler::setFocusState(InputHandler::FocusStates state)
+//{
+//	return get()._setFocusState(state);
+//}
+//
+//void InputHandler::attachToWindow(GLFWwindow * window)
+//{
+//	get()._attachToWindow(window);
+//}
+
+//void InputHandler::_synchronize()
+//{
+//	for (int i = 0; i < GLFW_MOUSE_BUTTON_LAST; i++)
+//	{
+//		previousMouseState[i] = glfwGetMouseButton(m_window, i);
+//	}
+//	for (int i = 0; i < GLFW_KEY_LAST; i++)
+//	{
+//		previousKeyboardState[i] = glfwGetKey(m_window, i);
+//	}
+//}
+//
+//bool InputHandler::_getMouseButtonUp(int button)
+//{
+//	return (!glfwGetMouseButton(m_window, button) && previousMouseState[button]);
+//}
+//
+//bool InputHandler::_getMouseButton(int button)
+//{
+//	return glfwGetMouseButton(m_window, button);
+//}
+//
+//bool InputHandler::_getMouseButtonDown(int button)
+//{
+//	return (glfwGetMouseButton(m_window, button) && !previousMouseState[button]);
+//}
+//
+//bool InputHandler::_getKeyUp(int key)
+//{
+//	return (!glfwGetKey(m_window, key) && previousKeyboardState[key]);
+//}
+//
+//bool InputHandler::_getKey(int key)
+//{
+//	return glfwGetKey(m_window, key);
+//}
+//
+//bool InputHandler::_getKeyDown(int key)
+//{
+//	return (glfwGetKey(m_window, key) && !previousKeyboardState[key]);
+//}
+//
+//InputHandler::FocusStates InputHandler::_getFocusState()
+//{
+//	return m_focusState;
+//}
+//
+//void InputHandler::_setFocusState(InputHandler::FocusStates state)
+//{
+//	m_focusState = state;
+//}
+//
+//void InputHandler::_attachToWindow(GLFWwindow * window)
+//{
+//	m_window = window;
+//}
--- tp02/InputHandler.h
+++ tp02/InputHandler.h
@@ -5,49 +5,64 @@
 #include "glew/glew.h"
 #include "GLFW/glfw3.h"
 
+#include "Utils.h"
+
 class InputHandler
 {
-private :
-	GLFWwindow* m_window;
+public:
+	enum FocusStates {
+		FOCUSING_NONE = 0,
+		FOCUSING_GAME = 1 << 0,
+		FOCUSING_HUD = 1 << 1,
+		FOCUSING_EDITOR = 1 << 2,
+		FOCUSING_ALL = FOCUSING_EDITOR | FOCUSING_GAME | FOCUSING_HUD,
+	};
 
-	std::map<int, bool> previousMouseState;
-	std::map<int, bool> previousKeyboardState;
+private :
+	static GLFWwindow* m_window;
+
+	static std::map<int, bool> previousMouseState;
+	static std::map<int, bool> previousKeyboardState;
+
+	static FocusStates m_focusState;
 
 public:
 
 	static void synchronize();
 
-	static bool getMouseButtonUp(int button);
-	static bool getMouseButton(int button);
-	static bool getMouseButtonDown(int button);
+	static bool getMouseButtonUp(int button, FocusStates focusContext = FocusStates::FOCUSING_ALL);
+	static bool getMouseButton(int button, FocusStates focusContext = FocusStates::FOCUSING_ALL);
+	static bool getMouseButtonDown(int button, FocusStates focusContext = FocusStates::FOCUSING_ALL);
 
-	static bool getKeyUp(int key);
-	static bool getKey(int key);
-	static bool getKeyDown(int key);
+	static bool getKeyUp(int key, FocusStates focusContext = FocusStates::FOCUSING_ALL);
+	static bool getKey(int key, FocusStates focusContext = FocusStates::FOCUSING_ALL);
+	static bool getKeyDown(int key, FocusStates focusContext = FocusStates::FOCUSING_ALL);
 
+	static FocusStates getFocusState();
+	static void setFocusState(FocusStates state);
 
 	static void attachToWindow(GLFWwindow* window);
 
 private:
-	void _synchronize();
+	//void _synchronize();
 
-	bool _getMouseButtonUp(int button);
-	bool _getMouseButton(int button);
-	bool _getMouseButtonDown(int button);
+	//bool _getMouseButtonUp(int button);
+	//bool _getMouseButton(int button);
+	//bool _getMouseButtonDown(int button);
 
-	bool _getKeyUp(int key);
-	bool _getKey(int key);
-	bool _getKeyDown(int key);
+	//bool _getKeyUp(int key);
+	//bool _getKey(int key);
+	//bool _getKeyDown(int key);
 
-	void _attachToWindow(GLFWwindow* window);
+	//FocusStates _getFocusState();
+	//void _setFocusState(FocusStates state);
+
+	//void _attachToWindow(GLFWwindow* window);
 
 
 // singleton implementation :
 private:
-	InputHandler() {
-
-	}
-
+	InputHandler();
 public:
 	inline static InputHandler& get()
 	{
@@ -61,3 +76,4 @@ public:
 	void operator=(const InputHandler& other) = delete;
 };
 
+ENUM_MASK_IMPLEMENTATION(InputHandler::FocusStates)
